8a5c0111d41a3bba74e19121ba746222790faae4
lchoros <choros777@gmail.com>
2019-09-06 12:31:06 +0100

    First exercises

---
+++ MinNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ SummingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SummingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
